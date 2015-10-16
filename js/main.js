8546ad394474726638ff9ad3a2b15c15f61e41f8
Graphic <graphic@SOD-SJH-516-34M.local>
2015-10-16 14:38:33 -0400

    yeah

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
-	
+
 	$('img[alt="Batman"]').addClass('shown');
 	$('.name').text('batman');
 
+
 });
+
+$('.next').click(function(){
+
+	$('img[alt="Batman"]').removeClass('shown');
+	$('img[alt="Batman"]').next().addClass('shown');
+	$('.name').text('captain america');
+
+});
+
